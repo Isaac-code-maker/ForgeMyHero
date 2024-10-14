3bccf26d1d06dfb2db94019f8f338c733cf1d263
Isaac Aires <83322345+Isaac-code-maker@users.noreply.github.com>
2024-10-14 19:15:01 -0300

    setup Forge My Hero project

---
+++ forgemyhero/src/test/java/com/forgemyhero/forgemyhero/ForgemyheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forgemyhero.forgemyhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgemyheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
